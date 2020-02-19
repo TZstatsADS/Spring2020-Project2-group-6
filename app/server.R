2dfd238eec1e4aa1e10c0a0244850dbfae96eddd
StephanieLoveData <36903167+Stephanielovdata@users.noreply.github.com>
2020-02-18 22:52:42 -0500

    change by li

--- app/server.R
+++ app/server.R
@@ -16,11 +16,18 @@ SS_17 <- read_csv('../data/School Survey 2017.csv')
 SL <- School_Locations%>%filter(Status_descriptions=='Open')%>%
   select(location_code,location_name,location_type_description,Location_Category_Description,
          primary_address_line_1,LONGITUDE,LATITUDE)
+
 QR_1519 <- QR%>%
   filter(Start_Date>='2015-01-01')%>%
-  select(BN,School_Year,Start_Date,Indicator_1.1,Indicator_1.2,Indicator_1.3,Indicator_1.4,
-         Indicator_2.2,Indicator_3.1,Indicator_3.4,Indicator_4.1,Indicator_4.2,Indicator_5.1)
+  select(BN,School_Year,Indicator_1.1,Indicator_1.2,Indicator_1.3,Indicator_1.4,
+         Indicator_2.2,Indicator_3.1,Indicator_3.4,Indicator_4.1,Indicator_4.2,Indicator_5.1)%>%
+  pivot_wider(names_from = School_Year,values_from = starts_with('Indicator'))
 
+bn_sl <- SL %>% select (location_code, location_name)
+QR_1519 %>%  merge(bn_sl, by.x=c("BN"),
+                   by.y=c("location_code")) %>% 
+  select(BN,location_name, School_Year,Indicator_1.1,Indicator_1.2,Indicator_1.3,Indicator_1.4,
+         Indicator_2.2,Indicator_3.1,Indicator_3.4,Indicator_4.1,Indicator_4.2,Indicator_5.1)
 
 shinyServer(function(input, output) {
   
