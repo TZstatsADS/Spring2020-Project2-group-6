39808b51d64a2e77708f91c87dd76d5ee65a764f
StephanieLoveData <36903167+Stephanielovdata@users.noreply.github.com>
2020-02-18 22:16:43 -0500

    change by li

--- app/server.R
+++ app/server.R
@@ -9,6 +9,7 @@ library(shinydashboard)
 library(tidyverse)
 
 load('../output/School_Locations.RData')
+load('../output/demographic_by_school.Rdata')
 QR <- read_csv('../data/2005_-_2019_Quality_Review_Ratings.csv')
 SS_17 <- read_csv('../data/School Survey 2017.csv')
 
@@ -18,8 +19,7 @@ SL <- School_Locations%>%filter(Status_descriptions=='Open')%>%
 QR_1519 <- QR%>%
   filter(Start_Date>='2015-01-01')%>%
   select(BN,School_Year,Start_Date,Indicator_1.1,Indicator_1.2,Indicator_1.3,Indicator_1.4,
-         Indicator_2.2,Indicator_3.1,Indicator_3.4,Indicator_4.1,Indicator_4.2,Indicator_5.1)%>%
-  pivot_wider()
+         Indicator_2.2,Indicator_3.1,Indicator_3.4,Indicator_4.1,Indicator_4.2,Indicator_5.1)
 
 
 shinyServer(function(input, output) {
@@ -28,7 +28,7 @@ shinyServer(function(input, output) {
     m <- leaflet() %>%
       addProviderTiles(providers$CartoDB.Positron) %>%
       setView(-73.9252853,40.7910694,zoom = 13)
-      leafletProxy("map", data = School_Locations) %>%
+      leafletProxy("map", data = SL) %>%
       addCircleMarkers(lng=~LONGITUDE,lat=~LATITUDE,popup=~location_name,radius=4,opacity=1,fillOpacity =1 ,stroke=F,color='#c4f1f2')
     
     m
